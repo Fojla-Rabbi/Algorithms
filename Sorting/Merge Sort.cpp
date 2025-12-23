a81dfb4e6bea62c930c1e8a6c07511329ac0289f
Fojla Rabbi <fojlarabbi018@gmail.com>
2025-12-23 17:29:55 +0600

    Update Merge Sort.cpp

--- Sorting/Merge Sort.cpp
+++ Sorting/Merge Sort.cpp
@@ -2,62 +2,41 @@
 using namespace std;
 #define ll long long
 
-// O(n) --- for a complete level (there is total -- log n -- levels)
-vector<int> merge(vector<int> l, vector<int> r) {
-    int i = 0;
-    int j = 0;
-    vector<int> res;
-    while(i < l.size() && j < r.size()) {
-        if(l[i] <= r[j]) {
-            res.push_back(l[i]);
-            i++;
-        }
-        else {
-            res.push_back(r[j]);
-            j++;
-        }
+vector<int> a;
+
+vector<int> merge(vector<int> &l, vector<int> &r) {
+    int n = l.size(), m = r.size();
+    vector<int> ans;
+    int i = 0, j = 0;
+    while(i < n && j < m) {
+        if(l[i] <= r[j]) ans.push_back(l[i++]);
+        else ans.push_back(r[j++]);
     }
-    // If one of the pointers doesn't reach the end
-    while(i < l.size()) {
-        res.push_back(l[i]);
-        i++;
-    }
-    while(j < r.size()) {
-        res.push_back(r[j]);
-        j++;
-    }
-    return res;
+
+    while(i < n) ans.push_back(l[i++]);
+    while(j < m) ans.push_back(r[j++]);
+    
+    return ans;
 }
-vector<int> v;
-int n;
-// O(log n)
+
 vector<int> merge_sort(int low, int high) {
-    if(low == high) { // When only one element
-        vector<int> t;
-        t.push_back(v[low]);
-        return t;
-    }
+    if(low == high) return {a[low]};
     int mid = (low + high) / 2;
-    vector<int> l = merge_sort(low, mid);
-    vector<int> r = merge_sort(mid + 1, high);
-    return merge(l, r);
+    vector<int> left = merge_sort(low, mid);
+    vector<int> right = merge_sort(mid + 1, high);
+
+    return merge(left, right);
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    int n;
     cin >> n;
     vector<int> t(n);
-    for(int i = 0; i < n; i++) {
-        cin >> t[i];
-    }
-    v = t;
-    vector<int> ans = merge_sort(0, n - 1);
-    for(int i = 0; i < n; i++) {
-        cout << ans[i] << " ";
-    }
+    for(int i = 0; i < n; i++) cin >> t[i];
+
+    a = t;
+    a = merge_sort(0, n - 1);
+    for(int i = 0; i < n; i++) cout << a[i];
+ 
     return 0;
 }
-
-
-
